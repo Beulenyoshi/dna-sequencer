37095f381acb934d108d4559f45ebee1915d79c4
Thomas Johannesmeyer <thomas@geeky.gent>
2018-03-28 21:11:55 +0200

    Parsing cl arguments

--- src/main.cpp
+++ src/main.cpp
@@ -22,6 +22,23 @@ int main(int argc, char **argv) {
     log("<file1> <file2> <number of threads> <output file>");
   } else {
 
+    // Assigning CL parameters to temporary variables
+    std::string input_file1 = argv[1];
+    std::string input_file2 = argv[2];
+    std::string output_file = argv[4];
+
+    // Parsing thread count including default fallback of one
+    int thread_count = 1;
+    try {
+      int temp_thread_count = std::stoi(argv[3]);
+
+      if (temp_thread_count > 1) {
+        thread_count = temp_thread_count;
+      }
+    } catch (std::exception const &e) {
+      log("Invalid thread count parameter. Falling back to 1 threaded execution");
+    }
+
   }
 
   return 0;
